fe2f5c43b2a2e7ff881ed5d1e5413dde40a08e3a
Secret Santa <iamsecretsanta124@protonmail.com>
2020-12-17 20:34:45 -0500

    Add fuji chainid

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -5,7 +5,8 @@ export declare enum ChainId {
     ROPSTEN = 3,
     RINKEBY = 4,
     GÖRLI = 5,
-    KOVAN = 42
+    KOVAN = 42,
+    FUJI = 43113
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
